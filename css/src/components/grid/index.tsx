1b86141d37df8e9395086e444ec82544557682b0
Ziyou Lee <136683721+liziyou2023@users.noreply.github.com>
2023-06-27 01:58:56 +0000

    feat: css

---
+++ css/src/components/grid/index.tsx
@@ -0,0 +1,15 @@
+
+export default function Grid() {
+    return <div className="wrapper">
+        <div className="item">
+            <div className="vertical-text">css2023</div>
+        </div>
+        <div className="item ">
+            <div className="vertical-text">css2023</div>
+        </div>
+        <div className="section">section</div>
+        <div className="square">square</div>
+        <div className="card">card</div>
+    
+    </div>
+}
